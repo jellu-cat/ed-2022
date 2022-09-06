3e2a27b85eaf5ca125459d3a862df9d98ba2a45c
Jellu <105118442+Debilesque@users.noreply.github.com>
2022-09-05 23:20:14 -0500

    added

---
+++ programas/ejercicio-1.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+void pyramid(){
+    int i, j;
+
+    for(j = 1; j < (5 + 1); j++){
+        printf("%i ", j);
+    }
+    printf("\n");
+
+}
+
+int main(void){
+
+    pyramid();
+}
