951d7b839046f1d93b0ce8cb45fac874296390a1
Jellu <105118442+Debilesque@users.noreply.github.com>
2022-09-21 07:45:41 -0500

    Source files added

--- programs/structures/vector.c
+++ programs/structures/vector.c
@@ -2,18 +2,31 @@
 #include <string.h>
 
 int main(){
-	
-	struct{
-		char* calle;
-		char* ciudad;
-		int num;
-	} domicilio;
-	
-	domicilio.calle = "Tecnológico",
-	domicilio.ciudad = "Querétaro";
-	domicilio.num = 57;
-	
-	printf("Ciudad: %s\n", domicilio.ciudad);
-	printf("Calle: %s\n", domicilio.calle);
-	printf("Número: %i", domicilio.num);
+
+    int n = 2;
+
+    struct{
+        char calle[50];
+        char ciudad[50];
+        int num;
+    } vector[n];
+
+    for( int i = 0; i < n; i++){
+        printf("\n\t--- PERSONA %i ---\n", (i + 1));
+
+        printf("\tciudad: ");
+        scanf("%s", &vector[i].ciudad);
+
+        printf("\tcalle: ");
+        scanf("%s", &vector[i].calle);
+
+        printf("\tnúmero: ");
+        scanf("%i", &vector[i].num);
+    }
+
+    printf("\n\t--- DATOS ---\n");
+
+    for( int i = 0; i < n; i++){
+        printf("%i -> Calle %s, #%i; %s\n", (i + 1), vector[i].calle, vector[i].num, vector[i].ciudad);
+    }
 }
