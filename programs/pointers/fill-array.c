17ef46f6b9bd2f0962fb145f5ed525938fe24c2d
Jellu <105118442+Debilesque@users.noreply.github.com>
2022-10-29 12:37:34 -0500

    tabs fixed

--- programs/pointers/fill-array.c
+++ programs/pointers/fill-array.c
@@ -4,73 +4,83 @@
 // P7 - 19-10-22 - No-repetidos
 
 int asks(int size){
-	do{
-		printf("-> Ingrese el tamaño del arreglo: ");
-		scanf("%i", &size);
-	}
-	while(size < 0);
-	// The opposite conditions
-	
-	return size;
+    
+    printf("-> Ingrese el tamaño del arreglo: ");
+    scanf("%i", &size);
+
+    while(size < 0){
+        printf("\t(!!) Número fuera de rango.\n");
+        
+        printf("-> Ingrese el tamaño del arreglo: ");
+        scanf("%i", &size);
+    }
+
+    return size;
 }
 int get(int x){
-	do{
-		printf("-> Ingrese un número entre 1 y 10: ");
-		scanf("%i", &x);
-	}
-	while(x < 0 || x > 10);
-	// The opposite conditions
-	
-	return x;
+
+    printf("-> Ingrese un número entre 1 y 10: ");
+    scanf("%i", &x);
+
+    while(x < 0 || x > 10){
+        printf("\t(!!) Número fuera de rango.\n");
+        
+        printf("-> Ingrese un número entre 1 y 10: ");
+        scanf("%i", &x);
+    }
+   
+    return x;
 }
 
 int compare(int x, int arr[], int size){
-	
-	int i = 0;
-	for (i = 0; i < size; i++){
-		
-		if(x == *(arr + i)){
-			printf("-> El número ya está en el arreglo.\n");
-			return -1;
-		}
-	}
-	return x;
+    
+    int i = 0;
+    for (i = 0; i < size; i++){
+   
+        if(x == *(arr + i)){
+            printf("\t(!!) El número ya está en el arreglo.\n");
+            return -1;
+        }
+    }
+    return x;
 }
 
 int parr(int arr[], int size){
-	
-	int i = 0;
-	printf("\n\n{");
-	for (i = 0; i < size; i++){
-		printf("%i,", *(arr + i));
-	}
-	printf("}\n\n");
+    
+    int i = 0;
+    printf("\n\n{");
+    for (i = 0; i < size; i++){
+        printf("%i,", *(arr + i));
+        if(i < (size - 1)){
+            printf("%i", *(arr + i));
+        }
+    }
+    printf("}\n\n");
 }
 
 int main(void){
-	
-	setlocale(LC_ALL,"");
-	
-	int size, n;
-	int* p;
-	
-	size = asks(size);
-	int arr[size];
-	
-	for(int i = 0; i < size; i++){
-		n = get(n);
-		n = compare(n, arr, size);
-	
-		while(n == -1){
-			n = get(n);
-			n = compare(n, arr, size);
-	
-		}
-		*(arr + i) = n;
-	}	
-	
-	parr(arr, size);
-	
-	return 0;
-	
+
+    setlocale(LC_ALL,"");
+    
+    int size, n;
+    int* p;
+    
+    size = asks(size);
+    int arr[size];
+    
+    for(int i = 0; i < size; i++){
+        n = get(n);
+        n = compare(n, arr, size);
+    
+        while(n == -1){
+            n = get(n);
+            n = compare(n, arr, size);
+    
+        }
+        *(arr + i) = n;
+    }
+    
+    parr(arr, size);
+    
+    return 0;
 }
--- programs/pointers/par.c
+++ programs/pointers/par.c
@@ -4,29 +4,29 @@
 // P6 - 18-10-22 - Par-Impar
 
 int par(int *p){
-	if( ((*p) % 2) == 0){
-		printf("-> El número es par.");
-	}
-	else{
-		printf("-> El número es impar.");
-	}
-	return 0;
+    if( ((*p) % 2) == 0){
+        printf("-> El número es par.");
+        }
+        else{
+        printf("-> El número es impar.");
+        }
+    return 0;
 }
 
 int main(void){
-	
-	setlocale(LC_ALL,"");
-	
-	int n, *p;
-	p = &n;
-	
-	printf("n = ");
-	scanf("%i", &n);
-	
-	printf("\n-> Número = %i\n",*p);
-	
-	par(p);
-	
-	return 0;
-	
+
+    setlocale(LC_ALL,"");
+    
+    int n, *p;
+    p = &n;
+    
+    printf("n = ");
+    scanf("%i", &n);
+    
+    printf("\n-> Número = %i\n",*p);
+    
+    par(p);
+    
+    return 0;
+    
 }
