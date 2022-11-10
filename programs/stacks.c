2a30147ee3587bcffbaeb52a334205cce621db05
jellu-cat <105118442+jellu-cat@users.noreply.github.com>
2022-11-10 11:59:43 -0600

    Add files via upload

---
+++ programs/stacks.c
@@ -0,0 +1,38 @@
+#include<stdio.h>
+
+// Pilas: Apilar y desapilar nodos
+// el primero que entra es el último que sale
+// estructura de datos de entradas ordenadas en las que sólo se pueden introducir y eliminar datos por la cola (extremo)
+
+// Operaciones de pila
+// Entrada y salida
+
+// Las pilas se componen de nodos
+// se componen de datos y punteros al siguiente elemento (el primero apunta a null)
+// no están contiguos en memoria
+
+// Creación del nodo de una pila
+struct node {
+	int x;
+	node *next;
+}
+
+// Creación del espacio de memoria para el nodo
+// Cargar dato al nodo
+// Cargar puntero
+// Asignar el nuevo nodo a la pila
+
+
+int main(void){
+	
+	// Crea la estructura
+	node *index = NULL;
+	
+	// Crea un nuevo nodo
+	node *newnode = new node();
+	
+	// Se atualiza el valor
+	
+	
+	return 0;
+}
